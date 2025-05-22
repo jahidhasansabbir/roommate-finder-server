e468a76d63d4481cfa5851ef5c6ddbf1279cdc9d
jahidhasansabbir1 <jahidhasansabbir1@gmail.com>
2025-05-22 22:09:07 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+// simpleDB:
+const uri =
+  "mongodb+srv://simpleDB:qCuLROAePBVx8cHy@cluster0.ythsg28.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+};
+run();
 
 app.get("/", (req, res) => {
   res.send("The roommate finder server is running...");
