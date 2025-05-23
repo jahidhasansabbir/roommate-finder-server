74a3eea56c2dd5bc59d311ac430bde1e661079b6
jahidhasansabbir1 <jahidhasansabbir1@gmail.com>
2025-05-23 23:04:01 +0600

    secured DB username & pass using dotenv

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-// simpleDB:
-const uri =
-  "mongodb+srv://simpleDB:qCuLROAePBVx8cHy@cluster0.ythsg28.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ythsg28.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
